feec7f264db1eac91f7a2747901ca37b9a17514d
Rabita <rabita.rowsan@g.bracu.ac.bd>
2023-11-21 04:18:23 +0600

    customize tshirt

--- public/assets/js/customize.js
+++ public/assets/js/customize.js
@@ -1,76 +1,145 @@
-let canvas = new fabric.Canvas("tshirt-canvas");
+// Create a single instance of fabric.Canvas
+const canvas = new fabric.Canvas("tshirt-canvas");
 
+// Function to update T-shirt design
 function updateTshirtImage(imageURL) {
-  fabric.Image.fromURL(imageURL, function (img) {
-    img.scaleToHeight(300);
-    img.scaleToWidth(300);
-    canvas.centerObject(img);
-    canvas.add(img);
-    canvas.renderAll();
-  });
-}
-
-// Update the TShirt color according to the selected color by the user
-document.getElementById("tshirt-color").addEventListener(
-  "change",
-  function () {
-    document.getElementById("tshirt-div").style.backgroundColor = this.value;
-  },
-  false
-);
-
-// Update the TShirt color according to the selected color by the user
-document.getElementById("tshirt-design").addEventListener(
-  "change",
-  function () {
-    // Call the updateTshirtImage method providing as first argument the URL
-    // of the image provided by the select
-    updateTshirtImage(this.value);
-  },
-  false
-);
-
-// When the user clicks on upload a custom picture
-document.getElementById("tshirt-custompicture").addEventListener(
-  "change",
-  function (e) {
-    var reader = new FileReader();
-
-    reader.onload = function (event) {
-      var imgObj = new Image();
-      imgObj.src = event.target.result;
-
-      // When the picture loads, create the image in Fabric.js
-      imgObj.onload = function () {
-        var img = new fabric.Image(imgObj);
-
+    fabric.Image.fromURL(imageURL, function (img) {
         img.scaleToHeight(300);
         img.scaleToWidth(300);
         canvas.centerObject(img);
+
+        // Add close and remove buttons to the design
+        addCloseAndRemoveButtons(img);
+
         canvas.add(img);
         canvas.renderAll();
-      };
+    });
+}
+
+// Function to add text to the canvas
+function addTextToCanvas() {
+    const text = $('#tshirt-text').val();
+    const font = $('#text-font').val();
+    const textColor = $('#text-color').val();
+
+    const canvasText = new fabric.IText(text, {
+        left: 50,
+        top: 50,
+        fontFamily: font,
+        fill: textColor,
+    });
+
+    // Add close and remove buttons to the text
+    addCloseAndRemoveButtons(canvasText);
+
+    // Add the text to the canvas
+    canvas.add(canvasText);
+}
+
+// Function to add close and remove buttons to the object
+function addCloseAndRemoveButtons(object) {
+    const closeButton = new fabric.Text('X', {
+        fontFamily: 'Arial',
+        fontSize: 16,
+        fill: 'red',
+        originX: 'right',
+        originY: 'top',
+        selectable: false,
+    });
+
+    const removeButton = new fabric.Text('Remove', {
+        fontFamily: 'Arial',
+        fontSize: 16,
+        fill: 'blue',
+        originX: 'left',
+        originY: 'top',
+        selectable: false,
+    });
+
+    closeButton.on('mousedown', function () {
+        canvas.remove(object);
+        canvas.remove(this);
+        canvas.renderAll();
+    });
+
+    removeButton.on('mousedown', function () {
+        canvas.remove(object);
+        canvas.remove(this);
+        canvas.renderAll();
+    });
+
+    object.on('selected', function () {
+        canvas.add(closeButton);
+        canvas.add(removeButton);
+    });
+
+    object.on('deselected', function () {
+        canvas.remove(closeButton);
+        canvas.remove(removeButton);
+    });
+}
+
+// Update the T-Shirt color according to the selected color by the user
+document.getElementById("tshirt-color").addEventListener("change", function () {
+    document.getElementById("tshirt-div").style.backgroundColor = this.value;
+}, false);
+
+// Update the T-Shirt design according to the selected design by the user
+document.getElementById("tshirt-design").addEventListener("change", function () {
+    updateTshirtImage(this.value);
+}, false);
+
+// When the user clicks on upload a custom picture
+document.getElementById("tshirt-custompicture").addEventListener("change", function (e) {
+    var reader = new FileReader();
+
+    reader.onload = function (event) {
+        var imgObj = new Image();
+        imgObj.src = event.target.result;
+
+        // When the picture loads, create the image in Fabric.js
+        imgObj.onload = function () {
+            var img = new fabric.Image(imgObj);
+            img.scaleToHeight(300);
+            img.scaleToWidth(300);
+            canvas.centerObject(img);
+
+            // Add close and remove buttons to the design
+            addCloseAndRemoveButtons(img);
+
+            canvas.add(img);
+            canvas.renderAll();
+        };
     };
 
     // If the user selected a picture, load it
     if (e.target.files[0]) {
-      reader.readAsDataURL(e.target.files[0]);
+        reader.readAsDataURL(e.target.files[0]);
     }
-  },
-  false
-);
+}, false);
 
-// When the user selects a picture that has been added and press the DEL key
-// The object will be removed !
-document.addEventListener(
-  "keydown",
-  function (e) {
+// When the user selects a picture that has been added and presses the DEL key
+// The object will be removed!
+document.addEventListener("keydown", function (e) {
     var keyCode = e.keyCode;
 
     if (keyCode == 46) {
-      console.log("Removing selected element on Fabric.js on DELETE key !");
-      canvas.remove(canvas.getActiveObject());
+        const activeObject = canvas.getActiveObject();
+        if (activeObject) {
+            canvas.remove(activeObject);
+            canvas.renderAll();
+        }
     }
-  },
-  false
-);
+}, false);
+
+// Function to update text color
+function updateTextColor() {
+    const selectedColor = $('#text-color').val();
+    $('#tshirt-text').css('color', selectedColor);
+}
+
+// Rest of your existing code...
+
+// Angular module and controller (if needed)...
+
+// Function to load fonts and paging (if needed)...
--- resources/views/customize.blade.php
+++ resources/views/customize.blade.php
@@ -7,42 +7,80 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.5.0/fabric.min.js"></script>
-@section('content')
-<label for="tshirt-design">Sample Design:</label>
-        <select id="tshirt-design">
-            <option value="">Select a sample designs ...</option>
-            <option value="https://cdn.ourcodeworld.com/public-media/gallery/gallery-5d5b0e95d294c.png">Batman</option>
-        </select>
-        
-         <label for="tshirt-color">T-Shirt Color:</label>
-        <select id="tshirt-color">
-            <!-- You can add any color with a new option and definings its hex code -->
-            <option value="#fff">White</option>
-            <option value="#000">Black</option>
-            <option value="#f00">Red</option>
-            <option value="#008000">Green</option>
-            <option value="#ff0">Yellow</option>
-        </select>
-        
-        <label for="tshirt-custompicture">Upload your own design:</label>
-        <input type="file" id="tshirt-custompicture" />
 
-      </div>
-  
-<div class="container h-100">
-  <div class="row h-100 justify-content-center align-items-center">
-    <!-- Create the container of the tool -->
-        <div id="tshirt-div" style="padding-left:0px;">
-            <img id="tshirt-backgroundpicture" src="https://cdn.ourcodeworld.com/public-media/gallery/gallery-5d5afd3f1c7d6.png" width="460" height=""/>
-            <div id="drawingArea" class="drawing-area">					
-                <div class="canvas-container">
-                    <canvas id="tshirt-canvas" width="440" height="530"></canvas>
+@section('content')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <label for="tshirt-design" class="form-label">Sample Design:</label>
+            <select id="tshirt-design" class="form-select mb-3">
+                <option value="">Select a sample design...</option>
+                <option value="https://cdn.ourcodeworld.com/public-media/gallery/gallery-5d5b0e95d294c.png">Batman</option>
+            </select>
+
+            <label for="tshirt-color" class="form-label">T-Shirt Color:</label>
+            <p> 
+                <input type="color" value="#ffffff" id="clr" onchange="updateTshirtColor()">
+                Pick Color for T-Shirt
+            </p>
+
+            <label for="tshirt-custompicture" class="form-label">Upload your own design:</label>
+            <input type="file" id="tshirt-custompicture" class="form-control mb-3" />
+            
+            <label for="tshirt-text" class="form-label">Add Text:</label>
+            <input type="text" id="tshirt-text" class="form-control mb-3" placeholder="Enter text">
+
+            <label for="text-color" class="form-label" style="display: inline-block; margin-right: 10px;">Text Color:</label>
+            <input type="color" value="#ffffff" id="text-color" onchange="updateTextColor()" style="display: inline-block; vertical-align: middle;">
+            <br>
+            
+            <label for="text-font" class="form-label" style="display: inline-block; margin-right: 10px;">Text Font:</label>
+            <select id="text-font" class="form-select mb-3" style="display: inline-block; vertical-align: middle;">
+                <option value="Arial">Arial</option>
+                <option value="Helvetica">Helvetica</option>
+                <option value="Times New Roman">Times New Roman</option>
+                <!-- Add more font options as needed -->
+            </select>
+
+            <button class="btn btn-primary" onclick="addTextToCanvas()">Add Text to Canvas</button>
+
+        </div>
+
+        <div class="col-md-6">
+            <div id="tshirt-div" class="text-center">
+                <img id="tshirt-backgroundpicture" src="https://cdn.ourcodeworld.com/public-media/gallery/gallery-5d5afd3f1c7d6.png" class="img-fluid border-0" alt="T-Shirt Image">
+                <div id="drawingArea" class="drawing-area">
+                    <div class="canvas-container">
+                        <canvas id="tshirt-canvas" width="440" height="530"></canvas>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
 <!-- Include jQuery from a CDN -->
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="{{ asset('assets/js/customize.js') }}"></script>
 
+<script>
+    function updateTshirtColor() {
+        const selectedColor = $('#clr').val();
+        $('#tshirt-div').css('background-color', selectedColor);
+    }
+
+    // Add event listener to update T-shirt design
+    $('#tshirt-design').change(function () {
+        updateTshirtImage($(this).val());
+    });
+
+    // Add event listener to update T-shirt color when the color picker changes
+    $('#clr').change(updateTshirtColor);
+    // Add event listener to update color viewer when the color is changed
+    $('#tshirt-color').change(function() {
+        const selectedColor = $(this).val();
+        $('#color-viewer').css('background-color', selectedColor);
+    });
+</script>
 
 @endsection
