661f972bcadeda5a9d47d994dc39f0b706dbd6b0
Rabita <rabita.rowsan@g.bracu.ac.bd>
2023-12-12 14:07:38 +0600

    final

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -2,12 +2,15 @@
 
   <div class="nav-overlay uk-navbar-left">
 
-    <a href="#">{{config('app.name')}}</a>
+    <a href="{{route('home')}}">{{config('app.name')}}</a>
 
     <ul class="uk-navbar-nav">
-      <li class="uk-active"><a href="#">Active</a></li>
-      <li><a href="#">Item</a></li>
-      @auth
+      <li class="uk-active"><a href="{{route('customize')}}">Customize T-Shirt</a></li>
+  </div>
+
+  <div class="nav-overlay uk-navbar-right">
+  <ul class="uk-navbar-nav">
+  @auth
       <li><a href="{{route('logout')}}">Logout</a></li>
       @else
       <li><a href="{{route('login')}}">Login</a></li>
@@ -15,25 +18,8 @@
       @endauth
     </ul>
     <span>@auth{{auth()->user()->name}}@endauth</span>
-    
   </div>
 
-  <div class="nav-overlay uk-navbar-right">
-
-    <a class="uk-navbar-toggle" uk-search-icon="ratio: 2" uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
-
-  </div>
-
-  <div class="nav-overlay uk-navbar-left uk-flex-1" hidden>
-
-    <div class="uk-navbar-item uk-width-expand">
-      <form class="uk-search uk-search-navbar uk-width-1-1">
-        <input class="uk-search-input" type="search" placeholder="Search..." autofocus>
-      </form>
-    </div>
-
-    <a class="uk-navbar-toggle" uk-close uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
-
-  </div>
+  
 
 </nav>
